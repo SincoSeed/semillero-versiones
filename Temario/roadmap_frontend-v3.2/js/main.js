9232df0dc3079bdfd3454c632121f6b98ef17c3b
cmrow <cmrodriguez261@misena.edu.co>
2020-03-11 12:24:23 -0500

    Add carpeta para versión 3.2

---
+++ Temario/roadmap_frontend-v3.2/js/main.js
@@ -0,0 +1,27 @@
+
+var tarjetas = document.querySelectorAll('.tarjeta')
+tarjetas.forEach(element => element.addEventListener('click', selecionarMenu));
+var opcion;
+function selecionarMenu() {
+     opcion = this.classList[1];
+    //console.log(opcion);
+    //algoooo
+    
+    localStorage.setItem('key',opcion);
+    
+    window.location.assign('pages/Pprincipal.html');
+}
+
+const colapse = document.querySelectorAll('.colapse');
+const desplegar = document.querySelectorAll('.ltbuttom');
+mostrar(colapse);
+mostrar(desplegar);
+function mostrar(record) {
+    for (let i = 0; i < record.length; i++) {
+        record[i].onclick = function () {
+            //console.log(record[i]);
+            this.classList.toggle('active');
+            this.nextElementSibling.classList.toggle('ver');
+        }
+    }
+}
